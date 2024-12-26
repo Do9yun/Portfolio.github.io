dad4a5c05060d4172946a8dd7dce4c909668a952
Dogyun <57663482+Do9yun@users.noreply.github.com>
2024-12-26 17:09:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,17 @@ function showContent(section) {
 
     // 섹션별 내용 추가
     if (section === 'intro') {
-        contentArea.innerHTML += '<h2>내 정보</h2><p>내 정보에 대한 내용을 작성하세요.</p>';
+        contentArea.innerHTML += `
+            <h2>내 정보</h2>
+            <div class="info-section">
+                <img src="images/증명사진.jpg" alt="Profile Picture" class="profile-pic">
+                <div class="info-text">
+                    <p><strong>이름:</strong> 김도균</p>
+                    <p><strong>나이:</strong> 26세</p>
+                    <p><strong>소개:</strong> 저는 2D 이미지를 3D 메쉬로 변환하는 프로젝트를 진행하며 다양한 기술을 익히고 활용한 경험이 있습니다.</p>
+                </div>
+            </div>
+        `;
     } else if (section === 'resume') {
         contentArea.innerHTML += `
             <h2>자기소개서</h2>
